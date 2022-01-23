e4064fcbcb2ca85b9653c2ffc678442aac4557d4
enfow <wgm0601@gmail.com>
2022-01-23 17:35:22 +0900

    Drop Bio

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -30,9 +29,6 @@ const BlogPostTemplate = ({ data, location }) => {
           itemProp="articleBody"
         />
         <hr />
-        <footer>
-          <Bio />
-        </footer>
       </article>
       <nav className="blog-post-nav">
         <ul
